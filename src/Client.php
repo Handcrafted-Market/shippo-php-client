a79ec3304734f476cdb155baadc71abcd582c551
mweiner <mrweiner@gmail.com>
2021-11-27 19:16:28 -0800

    add rate+parcel+address service to client

--- src/Client.php
+++ src/Client.php
@@ -2,12 +2,40 @@
 
 namespace Handcrafted\Shippo;
 
+use Handcrafted\Shippo\Service\Parcel;
+use Handcrafted\Shippo\Service\Rate;
 use Handcrafted\Shippo\Service\Shipment;
+use \Handcrafted\Shippo\Service\Address;
 
 class Client extends ClientBase {
 
-  public function shipment() {
+  /**
+   * @return \Handcrafted\Shippo\Service\Shipment - The shipment service.
+   */
+  public function shipment(): Shipment {
     return new Shipment($this);
   }
+
+  /**
+   * @return \Handcrafted\Shippo\Service\Address - The address service.
+   */
+  public function address(): Address {
+    return new Address($this);
+  }
+
+  /**
+   * @return \Handcrafted\Shippo\Service\Parcel - The parcel service.
+   */
+  public function parcel(): Parcel {
+    return new Parcel($this);
+  }
+
+  /**
+   * @return \Handcrafted\Shippo\Service\Rate - The rate service.
+   */
+  public function rate(): Rate {
+    return new Rate($this);
+  }
+
 
 }
--- src/Service/Parcel.php
+++ src/Service/Parcel.php
@@ -38,6 +38,5 @@ class Parcel extends ServiceBase {
   \Handcrafted\Shippo\Resource\Pager {
     return $this->request('get', "/parcels");
   }
-
-
+  
 }
