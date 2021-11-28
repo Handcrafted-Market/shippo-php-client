a4b6d49dae3009af8f99816cd48f402cac362370
mweiner <mrweiner@gmail.com>
2021-11-27 17:54:57 -0800

    flesh out parcel and rate resource props

--- src/Resource/Parcel.php
+++ src/Resource/Parcel.php
@@ -4,4 +4,84 @@ namespace Handcrafted\Shippo\Resource;
 
 class Parcel extends ResourceBase {
 
+  /**
+   * @var string
+   */
+  protected $objectState;
+
+  /**
+   * @var string
+   */
+  protected $objectCreated;
+
+  /**
+   * @var string
+   */
+  protected $objectUpdated;
+
+  /**
+   * @var string
+   */
+  protected $objectId;
+
+  /**
+   * @var string
+   */
+  protected $objectOwner;
+
+  /**
+   * @var mixed|null
+   */
+  protected $template;
+
+  /**
+   * @var string
+   */
+  protected $length;
+
+  /**
+   * @var string
+   */
+  protected $width;
+
+  /**
+   * @var string
+   */
+  protected $height;
+
+  /**
+   * @var string
+   */
+  protected $distanceUnit;
+
+  /**
+   * @var string
+   */
+  protected $weight;
+
+  /**
+   * @var string
+   */
+  protected $massUnit;
+
+  /**
+   * @var string
+   */
+  protected $metadata;
+
+  /**
+   * @var mixed
+   */
+  protected $extra;
+
+  /**
+   * @var array
+   */
+  protected $lineItems;
+
+  /**
+   * @var bool
+   */
+  protected $test;
+
 }
--- src/Resource/Rate.php
+++ src/Resource/Rate.php
@@ -4,4 +4,99 @@ namespace Handcrafted\Shippo\Resource;
 
 class Rate extends ResourceBase {
 
+  /**
+   * @var string
+   */
+  protected $objectCreated;
+
+  /**
+   * @var string
+   */
+  protected $objectId;
+
+  /**
+   * @var string
+   */
+  protected $objectOwner;
+
+  /**
+   * @var string
+   */
+  protected $shipment;
+
+  /**
+   * @var array
+   */
+  protected $attributes;
+
+  /**
+   * @var string
+   */
+  protected $amountLocal;
+
+  /**
+   * @var string
+   */
+  protected $currencyLocal;
+
+  /**
+   * @var string
+   */
+  protected $amount;
+
+  /**
+   * @var string
+   */
+  protected $currency;
+
+  /**
+   * @var string
+   */
+  protected $provider;
+
+  /**
+   * @var string
+   */
+  protected $providerImage75;
+
+  /**
+   * @var string
+   */
+  protected $providerImage200;
+
+  /**
+   * @var \stdClass|mixed
+   */
+  protected $serviceLevel;
+
+  /**
+   * @var integer
+   */
+  protected $estimatedDays;
+
+  /**
+   * @var string
+   */
+  protected $durationTerms;
+
+  /**
+   * @var string
+   */
+  protected $carrierAccount;
+
+  /**
+   * @var string
+   */
+  protected $zone;
+
+  /**
+   * @var array
+   */
+  protected $messages;
+
+  /**
+   * @var bool
+   */
+  protected $test;
+
 }
