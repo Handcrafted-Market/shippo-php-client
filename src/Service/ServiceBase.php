d77d6c049a54584fe5780260f9c31a4476e64829
Matthew Weiner <mrweiner@gmail.com>
2022-08-17 13:49:30 -0700

    Request method throws guzzle exceptions

--- src/Service/ServiceBase.php
+++ src/Service/ServiceBase.php
@@ -5,16 +5,26 @@ namespace Handcrafted\Shippo\Service;
 use Handcrafted\Shippo\Client;
 
 abstract class ServiceBase {
-
-  /**
-   * @var \Handcrafted\Shippo\Client
-   */
+  
   public readonly Client $client;
 
   public function __construct(Client $client) {
     $this->client = $client;
   }
 
+  /**
+   * Makes a request.
+   *
+   * @param string $method
+   *   The request method.
+   * @param string $endpoint_uri
+   *   The endpoint uri to use for the request.
+   * @param array|null $params
+   *   The request params.
+   *
+   * @return \stdClass
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
   protected function request(string $method, string $endpoint_uri, ?array $params = NULL): \stdClass {
     return $this->client->request($method, $endpoint_uri, $params);
   }
